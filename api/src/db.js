a101dca0993e8d88f918b135b136f64cc0590880
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-03-12 20:26:07 -0300

    Nuevos comentarios en db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
+
 const fs = require('fs');
 const path = require('path');
+
+//----------Sequelize----------
+const { Sequelize } = require('sequelize');
+
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
@@ -10,6 +14,7 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+//-----------------------------
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -23,14 +28,14 @@ fs.readdirSync(path.join(__dirname, '/models'))
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
+// Pasamos los nombres de los modelos a que empiecen con mayúscula: country => Country
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Country } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
