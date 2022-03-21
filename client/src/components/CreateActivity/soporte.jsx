ff81960386477efe6e8b1875d70803256fe286a2
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-03-21 19:35:04 -0300

    Formulario funciona, falta que no acepte errores

---
+++ client/src/components/CreateActivity/soporte.jsx
@@ -0,0 +1,70 @@
+                    // <div className='inputSpecific1'>
+                    //     <label>Nombre de la actividad: </label>
+                    //     <input 
+                    //         type='text'
+                    //         name='name'
+                    //         onChange={(e) => handleInputChange(e)}
+                    //         value={activity.name}
+                    //     />
+                    //     {errors.name && (
+                    //         <p>{errors.name}</p>
+                    //     )}
+                    // </div>
+                    // <div className='inputSpecific2'>
+                    //     <label>Description: </label>
+                    //     <input
+                    //         type='text'
+                    //         name='description'
+                    //         onChange={(e) => handleInputChange(e)}
+                    //         value={activity.description}
+                    //     />
+                    //     {errors.description && (
+                    //         <p>{errors.description}</p>
+                    //     )}
+                    // </div>
+                    // <div className='inputComponent'>
+                    //     <label>Dificultad: </label>
+                    //     <input className='indputGeneric'
+                    //         type='number'
+                    //         name='difficulty'
+                    //         onChange={(e) => handleInputChange(e)}
+                    //         value={activity.difficulty}
+                    //     />
+                    //     <p>Nivel 1 representa la menor dificultad y Nivel 5 la mayor dificultad</p>
+                    //     {errors.difficulty && (
+                    //         <p>{errors.difficulty}</p>
+                    //     )}
+                    // </div>
+                    // <div className='inputComponent'>
+                    //     <label>Duración (hs.): </label>
+                    //     <input 
+                    //         className='indputGeneric'
+                    //         type='number'
+                    //         name='duration'
+                    //         onChange={(e) => handleInputChange(e)}
+                    //         value={activity.duration}
+                    //     />
+                    //     {errors.duration && (
+                    //         <p>{errors.duration}</p>
+                    //     )}
+                    // </div>
+                    // <div className='inputComponent'>
+                    //     <label>Temporada ideal para realizarla: </label>
+                    //     <select 
+                    //         className='inputGeneric' 
+                    //         onChange={e => handleInputChange(e)}
+                    //         name='season'
+                    //         value={activity.season}>
+                    //         <option value='winter'>Invierno</option>
+                    //         <option value='summer'>Verano</option>
+                    //         <option value='spring'>Primavera</option>
+                    //         <option value='autumn'>Otoño</option>
+                    //     </select>
+                    //     {/* <input 
+                    //         className='indputGeneric'
+                    //         type='text'
+                    //         name='season'
+                    //         onChange={(e) => handleInputChange(e)}
+                    //         value={activity.season}
+                    //     /> */}
+                    // </div>
